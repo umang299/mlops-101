b96f2702cb0364e6401bd9eb45b4ce2fb1f1462e
umang.agarwal@codemonk.in <umang.agarwal@codemonk.in>
2023-03-18 18:37:52 +0530

    refactor: add __name__ == "__main__" loop

--- train.py
+++ train.py
@@ -16,8 +16,6 @@ def get_accuray(preds, labels):
     return count/len(preds)
 
 
-trainloader, testloader, train, test = get_data()
-
 
 # logging the training configrations to weights and biases
 config = {
@@ -28,41 +26,44 @@ config = {
 }
 
 
-net = SimpleCNN() # Load model
-loss_fn = nn.CrossEntropyLoss() # define loss functions
-optimizer = torch.optim.SGD(net.parameters(), lr= config['learning_rate'], momentum= config['momentum']) # setting hyperparameters
 
+if __name__ == "__main__":
+    trainloader, testloader, train, test = get_data()
 
-Loss, Acc, best_acc = [], [], 0.7
+    net = SimpleCNN() # Load model
+    loss_fn = nn.CrossEntropyLoss() # define loss functions
+    optimizer = torch.optim.SGD(net.parameters(), lr= config['learning_rate'], momentum= config['momentum']) # setting hyperparameters
 
-for i in range(0,80):
-    loss_, acc_,  count = 0, 0, 0
-    for img, label in tqdm(trainloader):
-        x = img
-        y = label
+    Loss, Acc, best_acc = [], [], 0.7
 
-        optimizer.zero_grad()
-        net.zero_grad()
+    for i in range(0,80):
+        loss_, acc_,  count = 0, 0, 0
+        for img, label in tqdm(trainloader):
+            x = img
+            y = label
 
-        output = net(x)
-        loss = loss_fn(output, y)
-        
-        loss.backward()
-        optimizer.step()
+            optimizer.zero_grad()
+            net.zero_grad()
 
-        preds = torch.argmax(output, dim=1)
-        acc = get_accuray(preds, label)
+            output = net(x)
+            loss = loss_fn(output, y)
+            
+            loss.backward()
+            optimizer.step()
 
-        loss_ = loss_ + loss
-        acc_ = acc_ + acc
-        count += 1
+            preds = torch.argmax(output, dim=1)
+            acc = get_accuray(preds, label)
 
-    # wandb.log({"loss": loss_/count, 
-    #            "Acc": acc_/count })
-    print({"loss": loss_/count, 
-               "Acc": acc_/count })
+            loss_ = loss_ + loss
+            acc_ = acc_ + acc
+            count += 1
 
-    if acc_/count > best_acc:
-        # wandb.run.summary["best_accuracy"] = acc_/count
-        torch.save(net.state_dict(), "model.pts")
-        best_accuracy = acc_
+        # wandb.log({"loss": loss_/count, 
+        #            "Acc": acc_/count })
+        print({"loss": loss_/count, 
+                "Acc": acc_/count })
+
+        if acc_/count > best_acc:
+            # wandb.run.summary["best_accuracy"] = acc_/count
+            torch.save(net.state_dict(), "model.pts")
+            best_accuracy = acc_
