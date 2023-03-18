75d622ed1bcf54d77529508933e92f606ff5984d
umang.agarwal@codemonk.in <umang.agarwal@codemonk.in>
2023-03-18 17:47:32 +0530

    add: model class

---
+++ model.py
@@ -0,0 +1,25 @@
+import torch.nn as nn
+
+
+# Defining a simple cnn model
+class SimpleCNN(nn.Module):
+    """Basic Cnn model"""
+
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5)
+        self.pool = nn.MaxPool2d(kernel_size=2)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+        self.fc1 = nn.Linear(in_features=16*5*5, out_features=120)
+        self.fc2 = nn.Linear(in_features=120, out_features=84)
+        self.fc3 = nn.Linear(in_features=84, out_features=10)
+
+
+    def forward(self, x):
+        x = self.pool(nn.functional.relu(self.conv1(x)))
+        x = self.pool(nn.functional.relu(self.conv2(x)))
+        x = x.view(-1, 16*5*5)
+        x = nn.functional.relu(self.fc1(x))
+        x = nn.functional.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
